d8cfb4f62104b85d0f4278ea72c16bbb556af40a
Betelgeuse42 <kravmaryfr1@gmail.com>
2015-08-15 17:22:42 +0300

    second commit

--- risunki.m
+++ risunki.m
@@ -7,3 +7,4 @@ figurationlast('2_very small area');
 % figurationlast('3_very small area');
 % figurationlast('6_very small area');
 % % figurationlast('AK_1200_v300_50ds_920_V300_15ds');
+%%knblknbs'lfknb'slfkn;
